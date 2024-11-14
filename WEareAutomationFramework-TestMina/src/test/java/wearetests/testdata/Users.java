3a7dbc2d847bddb8adb38152ab54d6248fec6eee
mml-2 <mina.mladenova.a61@learn.telerikacademy.com>
2024-11-15 00:04:31 +0200

    Add files via upload
    First Commit with CreateAPost tests

---
+++ WEareAutomationFramework-TestMina/src/test/java/wearetests/testdata/Users.java
@@ -0,0 +1,13 @@
+package wearetests.testdata;
+
+public class Users {
+
+    public static final String VALID_USER="validUser";
+    public static final String INVALID_USER= "!@$!#@";
+    public static final String VALID_PASSWORD="vald!d!@#12*";
+    public static final String INVALID_PASSOWRD="12";
+    public static final String ADMIN_USER="taset@test.com";
+    public static final String ADMIN_PASSWORD="admin1";
+    public static final String USERNAME_VALID="validUser";
+    public static final String PASSWORD_VALID="vald!d!@#12*";
+}
